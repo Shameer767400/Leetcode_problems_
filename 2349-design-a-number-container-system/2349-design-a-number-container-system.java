60179ef934b1f94b86a4a961e5f896c69d82d84f
Shameer Meerambigari <shameeritsme21@gmail.com>
2026-03-01 00:18:07 +0530

    Update 2349-design-a-number-container-system.java

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.java
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.java
@@ -5,30 +5,30 @@ class NumberContainers {
     private Map<Integer, Integer> indexToNumber;
     private Map<Integer, PriorityQueue<Integer>> numberToIndices;
 
-    public NumberContainers() {
+    public NumberContainers()
+{
         indexToNumber = new HashMap<>();
         numberToIndices = new HashMap<>();
     }
-
-    public void change(int index, int number) {
+    public void change(int index, int number)
+{
         indexToNumber.put(index, number);
         numberToIndices
             .computeIfAbsent(number, k -> new PriorityQueue<>())
             .offer(index);
     }
-
-    public int find(int number) {
+    public int find(int number)
+{
         PriorityQueue<Integer> heap = numberToIndices.get(number);
         if (heap == null) return -1;
-
-        while (!heap.isEmpty()) {
+        while (!heap.isEmpty())
+{
             int idx = heap.peek();
             if (indexToNumber.get(idx) == number) {
                 return idx;
             }
             heap.poll(); // stale index
         }
-
         return -1;
     }
 }
